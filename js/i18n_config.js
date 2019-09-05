6c1927a3eca5ab4265f9399674e1863f670e2e2e
cacaudev <cacauabreu2013@gmail.com>
2019-09-04 23:39:06 -0300

    [feat] (Homepage): Fixed i18n config and add language menu

--- js/i18n_config.js
+++ js/i18n_config.js
@@ -1,43 +1,51 @@
-var set_locale_to = function (locale) {
-    if (locale)
-        jQuery.i18n().locale = locale;
-    jQuery('body').i18n();
-};
+(function($) {
+    "use strict"; // Start of use strict
 
-jQuery(function () {
-    jQuery.extend(jQuery.i18n.parser.emitter, {
-        sitename: () => "App To Test jquery"
+    var set_locale_to = function (locale) {
+        if (locale)
+            $.i18n().locale = locale;
+        $('body').i18n();
+    };
+
+    $(function () {
+        $.extend($.i18n.parser.emitter, {
+            sitename: function() {
+                return "App To Test $";
+            } 
+        });
     });
-});
 
-jQuery(function (jQuery) {
-    jQuery.i18n().load({
-        'en': './i18n/en.json',
-        'pt': './i18n/pt.json'
-    }).done(function () {
-        console.log('Languages loaded!');
+    $(function ($) {
+        $.i18n().load({
+            'en': './i18n/en.json',
+            'pt': './i18n/pt.json'
+        }).done(function () {
+            console.log('Languages loaded!');
 
-        // Browser default language
-        var userLang = navigator.language || navigator.userLanguage;
-        userLang = userLang.split('-')[0]; // In case the locale is like 'en-US', 'pt-BR'
+            // Browser default language
+            var userLang = navigator.language || navigator.userLanguage;
+            userLang = userLang.split('-')[0]; // In case the locale is like 'en-US', 'pt-BR'
 
-        console.log('Browser language ', userLang);
+            console.log('Browser language ', userLang);
 
-        if (userLang) set_locale_to(navigator.language)
+            if (userLang) set_locale_to(navigator.language)
 
-        // url(?locale) =>  takes the value of the locale GET parameter
-        set_locale_to(url('?locale'));
-
-        // supports HTML5 History/State APIs (persist locale)
-        History.Adapter.bind(window, 'statechange', function () {
+            // url(?locale) =>  takes the value of the locale GET parameter
             set_locale_to(url('?locale'));
-        });
 
-        // Change the locale upon click event on button
-        jQuery('.switch-locale').on('click', 'a', function (e) {
-            e.preventDefault();
-            //jQuery.i18n().locale = jQuery(this).data('locale');
-            History.pushState(null, null, "?locale=" + jQuery(this).data('locale'));
+            // supports HTML5 History/State APIs (persist locale)
+            History.Adapter.bind(window, 'statechange', function () {
+                set_locale_to(url('?locale'));
+            });
+
+            // Change the locale upon click event on button
+            $('.switch-locale').on('click', 'a', function (e) {
+                e.preventDefault();
+                //$.i18n().locale = $(this).data('locale');
+                History.pushState(null, null, "?locale=" + $(this).data('locale'));
+            });
         });
     });
-});
+
+
+})(jQuery); // End of use strict
